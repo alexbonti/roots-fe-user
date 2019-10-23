dcf6fe72a2b66d7dc8236127297ec98629808878
Andrea Mele <andrea.mele.au@gmail.com>
2019-10-23 13:37:15 +1100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import EnhancedNotification, { notify } from './common/Notification';
+import { DevModeSwitch } from './common/devModeSwitch'
+import { Header } from './dependants/Header'
+import { SideMenuItems } from './dependants/SideMenuItems'
+
+export {
+  EnhancedNotification as Notification,
+  notify,
+  DevModeSwitch,
+  Header,
+  SideMenuItems
+}
